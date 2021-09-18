7be3abe40ff1248923d50b752b9695f143cc7a05
ushrestha1 <61173612+ushrestha1@users.noreply.github.com>
2021-09-17 21:09:28 -0500

    Update TestUj.java
    2nd editied

--- TestProject/src/ujProject/TestUj.java
+++ TestProject/src/ujProject/TestUj.java
@@ -4,6 +4,6 @@ public class TestUj {
 public static void main(String[] args) {
 	System.out.println("hello guys");
 	
-	System.out.println("hello give me your phone number";
+	System.out.println("hello give me your phone number");
 }
 }
