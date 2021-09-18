63094e6021f64f842f197f9dba7d9315d3629c28
ushrestha1 <61173612+ushrestha1@users.noreply.github.com>
2021-09-17 21:03:48 -0500

    Update TestUj.java
    from github website

--- TestProject/src/ujProject/TestUj.java
+++ TestProject/src/ujProject/TestUj.java
@@ -3,5 +3,7 @@ package ujProject;
 public class TestUj {
 public static void main(String[] args) {
 	System.out.println("hello guys");
+	
+	System.out.println("hello give me your phone number";
 }
 }
